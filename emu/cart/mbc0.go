45ffe4d70aa1658ebb64118942cb2b46b9b9de6d
is386 <is386@drexel.edu>
2022-02-10 00:58:09 -0500

    add RAM to mbc0

--- emu/cart/mbc0.go
+++ emu/cart/mbc0.go
@@ -2,6 +2,7 @@ package cart
 
 type MBC0 struct {
 	ROM [0x7FFF - 0x0000 + 1]uint8
+	RAM [0x2000]uint8
 }
 
 func NewMBC0() MBC {
@@ -9,11 +10,18 @@ func NewMBC0() MBC {
 }
 
 func (m *MBC0) readByte(addr uint16) uint8 {
-	return m.ROM[addr]
+	if addr <= 0x7FFF {
+		return m.ROM[addr]
+	} else if (addr >= 0xA000) && (addr <= 0xBFFF) {
+		return m.RAM[addr-0xA000]
+	}
+	return 0x00
 }
 
 func (m *MBC0) writeROM(addr uint16, val uint8) {
 	m.ROM[addr] = val
 }
 
-func (m *MBC0) writeRAM(addr uint16, val uint8) {}
+func (m *MBC0) writeRAM(addr uint16, val uint8) {
+	m.RAM[addr-0xA000] = val
+}
