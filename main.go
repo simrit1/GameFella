cc9b79167a2ee342fb59de620a62a134aae3edc4
is386 <is386@drexel.edu>
2022-02-10 10:00:45 -0500

    add flag for debugging mode

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 // TODO: Flag to change color
-// TODO: Flag to output cpu debugging
 // TODO: Pass MBC1 Tests
 // TODO: MBC2
 // TODO: MBC3
@@ -15,27 +14,32 @@ import (
 	"github.com/is386/GoBoy/emu"
 )
 
-var (
-	DEBUG = false
-	ROM   = "roms/zelda.gb"
-)
-
-func parseArgs() string {
+func parseArgs() (string, bool) {
 	parser := argparse.NewParser("GameFella", "A simple GameBoy emulator written in Go.")
+
 	romFile := parser.File("r", "rom", os.O_RDWR, 0600,
 		&argparse.Options{
 			Required: true,
 			Help:     "Path to ROM file"})
+
+	debugFlag := parser.Flag("d", "debug",
+		&argparse.Options{
+			Required: false,
+			Help:     "Turns on debugging mode",
+			Default:  false,
+		})
+
 	err := parser.Parse(os.Args)
 	if err != nil {
 		fmt.Print(parser.Usage(err))
 		os.Exit(0)
 	}
-	return romFile.Name()
+
+	return romFile.Name(), *debugFlag
 }
 
 func main() {
-	rom := parseArgs()
-	gb := emu.NewGameBoy(rom, DEBUG)
+	rom, debug := parseArgs()
+	gb := emu.NewGameBoy(rom, debug)
 	gb.Run()
 }
